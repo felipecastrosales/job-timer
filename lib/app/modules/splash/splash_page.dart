ab3a69a26cc0904549d04f052277fea0b324a4a8
Felipe Sales <soufeliposales@gmail.com>
2022-06-05 23:21:49 -0300

    setting icons, assets and building splash

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -6,10 +6,19 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff0092b9),
+              Color(0xff0167b2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Image.asset('assets/images/logo.png'),
+        ),
       ),
-      body: Container(),
     );
   }
 }
