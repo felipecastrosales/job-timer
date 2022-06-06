c911b5fc8bc5a919d4217883e41f3ff33e1641db
Felipe Sales <soufeliposales@gmail.com>
2022-06-06 15:02:43 -0300

    adjusting details and building login screen

--- lib/app/modules/login/login_module.dart
+++ lib/app/modules/login/login_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'login_page.dart';
 
 
 class LoginModule extends Module {
@@ -11,7 +11,7 @@ class LoginModule extends Module {
   List<ModularRoute> get routes => [
         ChildRoute(
           '/',
-          child: (context, args) => const SplashPage(),
+          child: (context, args) => const LoginPage(),
         ),
       ];
 }
--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -5,11 +5,44 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff0092b9),
+              Color(0xff0167b2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              SizedBox(
+                height: screenSize.height * .1,
+              ),
+              SizedBox(
+                height: 49,
+                width: screenSize.width * .8,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(primary: Colors.grey[200]),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Image.asset('assets/images/google.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
